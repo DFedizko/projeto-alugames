3a4cfee5d57e77ac00299c5c3f50e5192d2a42ec
DFedizko <fedizkopedro@gmail.com>
2024-09-27 12:15:16 -0300

    adicionando total de jogos alugados no console

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 // O método utilizado aqui é recuperar o id do elemento pai do botão e da imagem (tag li) e dentro desse id, recuperar as classes para conseguir acessar o botão e a imagem
+let jogosAlugados = 0;
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let imagemJogo = jogo.querySelector('.dashboard__item__img')
     let botaoAlugarDevolver = jogo.querySelector('.dashboard__item__button');
-    let nomeJogo = jogo.querySelector('.dashboard__item__name')
+    let nomeJogo = jogo.querySelector('.dashboard__item__name');
     if (botaoAlugarDevolver.classList.contains('dashboard__item__button--return')) {
         if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
             imagemJogo.classList.remove('dashboard__item__img--rented');
             botaoAlugarDevolver.classList.remove('dashboard__item__button--return');
-            botaoAlugarDevolver.innerHTML = 'Alugar';   
+            botaoAlugarDevolver.innerHTML = 'Alugar';
+            jogosAlugados--;
         }
     } else {
         imagemJogo.classList.add('dashboard__item__img--rented');
         botaoAlugarDevolver.classList.add('dashboard__item__button--return');
         botaoAlugarDevolver.innerHTML = 'Devolver';
+        jogosAlugados++;
     }
+    exibirTotalJogosAlugados();
 }
+
+function exibirTotalJogosAlugados() {
+    console.log(`${jogosAlugados} jogos alugados`);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__button--return').length;
+});
